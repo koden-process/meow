cd4db43874892537ae858992f40fd1aa5ea38cb9
sheligon <heligon.seb@gmail.com>
2025-08-11 16:39:35 +0200

    issue #41
    Changement du nom de longlet comptes en répertoire et au differentes pages lié

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -1,96 +1,97 @@
-import { useEffect, useRef, useState, MouseEvent } from 'react';
-import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
-import { ActionType } from '../actions/Actions';
-import { selectCurrency, selectUserId, store } from '../store/Store';
-import { Avatar } from './Avatar';
-import { IconActivity } from './IconActivity';
-import { Translations } from '../Translations';
-import { DEFAULT_LANGUAGE } from '../Constants';
+import {useEffect, useRef, useState, MouseEvent} from 'react';
+import {useSelector} from 'react-redux';
+import {Link} from 'react-router-dom';
+import {ActionType} from '../actions/Actions';
+import {selectCurrency, selectUserId, store} from '../store/Store';
+import {Avatar} from './Avatar';
+import {IconActivity} from './IconActivity';
+import {Translations} from '../Translations';
+import {DEFAULT_LANGUAGE} from '../Constants';
 
 export const Navigation = () => {
-  const userId = useSelector(selectUserId);
-  const currency = useSelector(selectCurrency);
-  const [userMenue, setUserMenu] = useState(false);
-  const layerRef = useRef<HTMLDivElement>(null);
+    const userId = useSelector(selectUserId);
+    const currency = useSelector(selectCurrency);
+    const [userMenue, setUserMenu] = useState(false);
+    const layerRef = useRef<HTMLDivElement>(null);
 
-  const logout = () => {
-    store.dispatch({
-      type: ActionType.LOGOUT,
-    });
-  };
+    const logout = () => {
+        store.dispatch({
+            type: ActionType.LOGOUT,
+        });
+    };
 
-  function handleLinkClick(event: React.MouseEvent<HTMLAnchorElement>) {
-    setUserMenu(false);
-
-    return true;
-  }
-
-  useEffect(() => {
-    function handleClickOutside(event: MouseEvent<Document>) {
-      if (layerRef.current && !layerRef.current.contains(event.target as Node)) {
+    function handleLinkClick(event: React.MouseEvent<HTMLAnchorElement>) {
         setUserMenu(false);
-      }
+
+        return true;
     }
 
-    const clickHandler: EventListenerOrEventListenerObject =
-      handleClickOutside as unknown as EventListenerOrEventListenerObject;
+    useEffect(() => {
+        function handleClickOutside(event: MouseEvent<Document>) {
+            if (layerRef.current && !layerRef.current.contains(event.target as Node)) {
+                setUserMenu(false);
+            }
+        }
 
-    document.addEventListener('mousedown', clickHandler);
+        const clickHandler: EventListenerOrEventListenerObject =
+            handleClickOutside as unknown as EventListenerOrEventListenerObject;
 
-    return () => {
-      document.removeEventListener('mousedown', clickHandler);
-    };
-  }, [layerRef]);
+        document.addEventListener('mousedown', clickHandler);
 
-  return (
-    <>
-      <div className="item">
-        <Link to="/" title={Translations.OpportunitiesNavItem[DEFAULT_LANGUAGE]}>
-          <img alt={Translations.OpportunitiesNavItem[DEFAULT_LANGUAGE]} src={`/${currency?.toLocaleLowerCase()}-icon.svg`} />
-        </Link>
-      </div>
-      <div className="item">
-        <Link to="/activity" title={Translations.ActivitiesNavItem[DEFAULT_LANGUAGE]}>
-          <span className="icon">
-            <IconActivity />
-          </span>
-        </Link>
-      </div>
-      <div className="item">
-        <Link to="/forecast" title={Translations.ForecastNavItem[DEFAULT_LANGUAGE]}>
-          <img alt={Translations.ForecastNavItem[DEFAULT_LANGUAGE]} src="/forecast-icon.svg" />
-        </Link>
-      </div>
-      <div className="item">
-        <Link to="/accounts" title={Translations.AccountsNavItem[DEFAULT_LANGUAGE]}>
-          <img alt={Translations.AccountsNavItem[DEFAULT_LANGUAGE]} src="/accounts-icon.svg" />
-        </Link>
-      </div>
-      <div className="item">
-        <Link to="/hire" title={Translations.HireSpecialistNavItem[DEFAULT_LANGUAGE]}>
-          <img alt={Translations.HireSpecialistNavItem[DEFAULT_LANGUAGE]} src="/paw-icon.svg" />
-        </Link>
-      </div>
-      <div className="item">
-        <Link to="/setup" title={Translations.SetupNavItem[DEFAULT_LANGUAGE]}>
-          <img alt={Translations.SetupNavItem[DEFAULT_LANGUAGE]} src="/setup-icon.svg" />
-        </Link>
-      </div>
-      <div className="item" style={{ flexGrow: 1 }}></div>
-      <div className="user-menu">
-        {userMenue && (
-          <div className={userMenue ? 'wrapper' : ''} ref={layerRef}>
-            <Link onClick={handleLinkClick} to="/user-setup" className="link user-settings">
-              {Translations.SettingsNavItem[DEFAULT_LANGUAGE]}
-            </Link>
-            <div onClick={logout} className="link logout">
-              {Translations.LogoutButton[DEFAULT_LANGUAGE]}
+        return () => {
+            document.removeEventListener('mousedown', clickHandler);
+        };
+    }, [layerRef]);
+
+    return (
+        <>
+            <div className="item">
+                <Link to="/" title={Translations.OpportunitiesNavItem[DEFAULT_LANGUAGE]}>
+                    <img alt={Translations.OpportunitiesNavItem[DEFAULT_LANGUAGE]}
+                         src={`/${currency?.toLocaleLowerCase()}-icon.svg`}/>
+                </Link>
             </div>
-          </div>
-        )}
-        <Avatar onClick={() => setUserMenu(!userMenue)} width={36} id={userId} />
-      </div>
-    </>
-  );
+            <div className="item">
+                <Link to="/activity" title={Translations.ActivitiesNavItem[DEFAULT_LANGUAGE]}>
+          <span className="icon">
+            <IconActivity/>
+          </span>
+                </Link>
+            </div>
+            <div className="item">
+                <Link to="/forecast" title={Translations.ForecastNavItem[DEFAULT_LANGUAGE]}>
+                    <img alt={Translations.ForecastNavItem[DEFAULT_LANGUAGE]} src="/forecast-icon.svg"/>
+                </Link>
+            </div>
+            <div className="item">
+                <Link to="/accounts" title={Translations.DirectoryTitle[DEFAULT_LANGUAGE]}>
+                    <img alt={Translations.DirectoryTitle[DEFAULT_LANGUAGE]} src="/accounts-icon.svg"/>
+                </Link>
+            </div>
+            <div className="item">
+                <Link to="/hire" title={Translations.HireSpecialistNavItem[DEFAULT_LANGUAGE]}>
+                    <img alt={Translations.HireSpecialistNavItem[DEFAULT_LANGUAGE]} src="/paw-icon.svg"/>
+                </Link>
+            </div>
+            <div className="item">
+                <Link to="/setup" title={Translations.SetupNavItem[DEFAULT_LANGUAGE]}>
+                    <img alt={Translations.SetupNavItem[DEFAULT_LANGUAGE]} src="/setup-icon.svg"/>
+                </Link>
+            </div>
+            <div className="item" style={{flexGrow: 1}}></div>
+            <div className="user-menu">
+                {userMenue && (
+                    <div className={userMenue ? 'wrapper' : ''} ref={layerRef}>
+                        <Link onClick={handleLinkClick} to="/user-setup" className="link user-settings">
+                            {Translations.SettingsNavItem[DEFAULT_LANGUAGE]}
+                        </Link>
+                        <div onClick={logout} className="link logout">
+                            {Translations.LogoutButton[DEFAULT_LANGUAGE]}
+                        </div>
+                    </div>
+                )}
+                <Avatar onClick={() => setUserMenu(!userMenue)} width={36} id={userId}/>
+            </div>
+        </>
+    );
 };
--- frontend/src/pages/AccountsPage.tsx
+++ frontend/src/pages/AccountsPage.tsx
@@ -1,193 +1,195 @@
-import { Button } from '@adobe/react-spectrum';
-import { useEffect, useMemo } from 'react';
-import { useSelector } from 'react-redux';
-import { setListViewColumn, setListViewSortBy, showAccountLayer } from '../actions/Actions';
-import { Layer as AccountLayer } from '../components/account/Layer';
+import {Button} from '@adobe/react-spectrum';
+import {useEffect, useMemo} from 'react';
+import {useSelector} from 'react-redux';
+import {setListViewColumn, setListViewSortBy, showAccountLayer} from '../actions/Actions';
+import {Layer as AccountLayer} from '../components/account/Layer';
 import {
-  selectAccounts,
-  selectInterfaceState,
-  selectSchemaByType,
-  selectView,
-  selectViewColumns,
-  store,
+    selectAccounts,
+    selectInterfaceState,
+    selectSchemaByType,
+    selectView,
+    selectViewColumns,
+    store,
 } from '../store/Store';
-import { ListViewHelper } from '../helpers/ListViewHelper';
-import { ApplicationStore } from '../store/ApplicationStore';
-import { Schema, SchemaType } from '../interfaces/Schema';
-import { toRelativeDate } from '../helpers/DateHelper';
-import { ListViewItem, DataRow } from '../interfaces/ListView';
-import { TableHeader } from '../components/view/table/TableHeader';
-import { TableCanvas } from '../components/view/table/TableCanvas';
-import { ListFilterCanvas } from '../components/view/ListFilterCanvas';
-import { ListSearchCanvas } from '../components/view/ListSearchCanvas';
+import {ListViewHelper} from '../helpers/ListViewHelper';
+import {ApplicationStore} from '../store/ApplicationStore';
+import {Schema, SchemaType} from '../interfaces/Schema';
+import {toRelativeDate} from '../helpers/DateHelper';
+import {ListViewItem, DataRow} from '../interfaces/ListView';
+import {TableHeader} from '../components/view/table/TableHeader';
+import {TableCanvas} from '../components/view/table/TableCanvas';
+import {ListFilterCanvas} from '../components/view/ListFilterCanvas';
+import {ListSearchCanvas} from '../components/view/ListSearchCanvas';
 import useMobileLayout from '../hooks/useMobileLayout';
-import { Account } from '../interfaces/Account';
-import { Item } from '../components/view/list/Item';
-import { Row } from '../components/view/table/Row';
-import { Layer as CardLayer } from '../components/card/Layer';
-import { Translations } from '../Translations';
-import { DEFAULT_LANGUAGE } from '../Constants';
+import {Account} from '../interfaces/Account';
+import {Item} from '../components/view/list/Item';
+import {Row} from '../components/view/table/Row';
+import {Layer as CardLayer} from '../components/card/Layer';
+import {Translations} from '../Translations';
+import {DEFAULT_LANGUAGE} from '../Constants';
 
 const createListViewItemsFromSchema = (schema: Schema | undefined): ListViewItem[] => {
-  const list = [
-    {
-      name: Translations.NameLabel[DEFAULT_LANGUAGE],
-      column: 'name',
-      isHidden: false,
-    },
-  ];
+    const list = [
+        {
+            name: Translations.NameLabel[DEFAULT_LANGUAGE],
+            column: 'name',
+            isHidden: false,
+        },
+    ];
 
-  schema?.attributes.map((attribute) => {
-    list.push({
-      name: attribute.name,
-      column: attribute.key,
-      isHidden: false,
+    schema?.attributes.map((attribute) => {
+        list.push({
+            name: attribute.name,
+            column: attribute.key,
+            isHidden: false,
+        });
     });
-  });
 
-  list.push({
-    name: Translations.CreatedAtLabel[DEFAULT_LANGUAGE],
-    column: 'createdAt',
-    isHidden: false,
-  });
+    list.push({
+        name: Translations.CreatedAtLabel[DEFAULT_LANGUAGE],
+        column: 'createdAt',
+        isHidden: false,
+    });
 
-  return list;
+    return list;
 };
 
 export const AccountsPage = () => {
-  const state = useSelector(selectInterfaceState);
-  const accounts = useSelector(selectAccounts);
-  const view = useSelector((store: ApplicationStore) => selectView(store, 'accounts'));
-  const columns = useSelector((store: ApplicationStore) => selectViewColumns(store, 'accounts'));
-  const isMobileLayout = useMobileLayout();
+    const state = useSelector(selectInterfaceState);
+    const accounts = useSelector(selectAccounts);
+    const view = useSelector((store: ApplicationStore) => selectView(store, 'accounts'));
+    const columns = useSelector((store: ApplicationStore) => selectViewColumns(store, 'accounts'));
+    const isMobileLayout = useMobileLayout();
 
-  const schema = useSelector((store: ApplicationStore) =>
-    selectSchemaByType(store, SchemaType.Account)
-  );
+    const schema = useSelector((store: ApplicationStore) =>
+        selectSchemaByType(store, SchemaType.Account)
+    );
 
-  useEffect(() => {
-    if (schema && columns.length === 0) {
-      store.dispatch(setListViewColumn('accounts', createListViewItemsFromSchema(schema)));
-    }
-  }, [schema]);
+    useEffect(() => {
+        if (schema && columns.length === 0) {
+            store.dispatch(setListViewColumn('accounts', createListViewItemsFromSchema(schema)));
+        }
+    }, [schema]);
 
-  const openAccount = (id?: string) => {
-    store.dispatch(showAccountLayer(id));
-  };
+    const openAccount = (id?: string) => {
+        store.dispatch(showAccountLayer(id));
+    };
 
-  const toDataRows = (list: Account[]) => {
-    return list.map((account) => {
-      const row: DataRow = {
-        id: account._id,
-        name: account.name,
-        createdAt: account.createdAt,
-      };
+    const toDataRows = (list: Account[]) => {
+        return list.map((account) => {
+            const row: DataRow = {
+                id: account._id,
+                name: account.name,
+                createdAt: account.createdAt,
+            };
 
-      schema?.attributes.map(({ key }) => {
-        row[key] = account.attributes?.[key];
-      });
-      return row;
-    });
-  };
+            schema?.attributes.map(({key}) => {
+                row[key] = account.attributes?.[key];
+            });
+            return row;
+        });
+    };
 
-  const rows = useMemo(() => {
-    const list = toDataRows(accounts);
+    const rows = useMemo(() => {
+        const list = toDataRows(accounts);
 
-    return ListViewHelper.filterAndOrder(list, columns, view);
-  }, [schema, view, accounts, columns]);
+        return ListViewHelper.filterAndOrder(list, columns, view);
+    }, [schema, view, accounts, columns]);
 
-  const getCell = (row: DataRow, item: ListViewItem) => {
-    switch (item.column) {
-      case 'name':
-        return (
-          <td>
+    const getCell = (row: DataRow, item: ListViewItem) => {
+        switch (item.column) {
+            case 'name':
+                return (
+                    <td>
             <span onClick={() => openAccount(row.id?.toString())} className="direct-link">
               {row.name}
             </span>
-          </td>
-        );
-      case 'createdAt':
-        return <td>{toRelativeDate(row.createdAt)}</td>;
-      default:
-        return <td>{item.column !== null && row[item.column]?.toString()}</td>;
-    }
-  };
+                    </td>
+                );
+            case 'createdAt':
+                return <td>{toRelativeDate(row.createdAt)}</td>;
+            default:
+                return <td>{item.column !== null && row[item.column]?.toString()}</td>;
+        }
+    };
 
-  const getListItem = (row: DataRow, item: ListViewItem) => {
-    switch (item.column) {
-      case 'name':
-        return (
-          <div key={item.column}>
+    const getListItem = (row: DataRow, item: ListViewItem) => {
+        switch (item.column) {
+            case 'name':
+                return (
+                    <div key={item.column}>
             <span onClick={() => openAccount(row.id?.toString())} className="direct-link title">
               {row.name}
             </span>
-          </div>
-        );
-      case 'createdAt':
-        return (
-          <div key={item.column}>
-            <b>{Translations.CreatedLabel[DEFAULT_LANGUAGE]}</b> {toRelativeDate(row.createdAt)}
-          </div>
-        );
-      default:
-        return item.column !== null && row[item.column] ? (
-          <div key={item.column}>
-            <b>{item.name}:</b> {row[item.column]}
-          </div>
-        ) : null;
-    }
-  };
-
-  return (
-    <>
-      {state === 'account-detail' && <AccountLayer />}
-      {state === 'card-detail' && <CardLayer />}
-
-      <div className="canvas">
-        <div className="list-view-header">
-          <div>
-            <h2>{Translations.AccountsTitle[DEFAULT_LANGUAGE]} {rows.length}</h2>
-            <div style={{ paddingLeft: '10px' }}>
-              <Button variant="primary" onPress={() => openAccount()}>
-                {Translations.AddButton[DEFAULT_LANGUAGE]}
-              </Button>
-            </div>
-          </div>
-          <div className="toolbar">
-            <ListSearchCanvas name="accounts" />
-            <ListFilterCanvas name="accounts" columns={columns} />
-          </div>
-        </div>
-
-        {isMobileLayout ? (
-          <div className="mobile-view">
-            {rows.map((row, index) => {
-              return (
-                <Item key={index}>
-                  {columns
-                    .filter(({ isHidden }) => isHidden === false)
-                    .map((item) => getListItem(row, item))}
-                </Item>
-              );
-            })}
-          </div>
-        ) : (
-          <div className="content-box" style={{ overflow: 'auto' }}>
-            <TableCanvas>
-              <TableHeader name="accounts" sort={setListViewSortBy} view={view} columns={columns} />
-              {rows.map((row, index) => {
-                return (
-                  <Row key={index}>
-                    {columns
-                      .filter(({ isHidden }) => isHidden === false)
-                      .map((item) => getCell(row, item))}
-                  </Row>
+                    </div>
                 );
-              })}
-            </TableCanvas>
-          </div>
-        )}
-      </div>
-    </>
-  );
+            case 'createdAt':
+                return (
+                    <div key={item.column}>
+                        <b>{Translations.CreatedLabel[DEFAULT_LANGUAGE]}</b> {toRelativeDate(row.createdAt)}
+                    </div>
+                );
+            default:
+                return item.column !== null && row[item.column] ? (
+                    <div key={item.column}>
+                        <b>{item.name}:</b> {row[item.column]}
+                    </div>
+                ) : null;
+        }
+    };
+
+    return (
+        <>
+            {state === 'account-detail' && <AccountLayer/>}
+            {state === 'card-detail' && <CardLayer/>}
+
+            <div className="canvas">
+                <div className="list-view-header">
+                    <div>
+                        <h2>
+                            {Translations.DirectoryTitle[DEFAULT_LANGUAGE]} - {Translations.NumberOfContacts[DEFAULT_LANGUAGE]}: {rows.length}
+                        </h2>
+                        <div style={{paddingLeft: '10px'}}>
+                            <Button variant="primary" onPress={() => openAccount()}>
+                                {Translations.AddButton[DEFAULT_LANGUAGE]}
+                            </Button>
+                        </div>
+                    </div>
+                    <div className="toolbar">
+                        <ListSearchCanvas name="accounts"/>
+                        <ListFilterCanvas name="accounts" columns={columns}/>
+                    </div>
+                </div>
+
+                {isMobileLayout ? (
+                    <div className="mobile-view">
+                        {rows.map((row, index) => {
+                            return (
+                                <Item key={index}>
+                                    {columns
+                                        .filter(({isHidden}) => isHidden === false)
+                                        .map((item) => getListItem(row, item))}
+                                </Item>
+                            );
+                        })}
+                    </div>
+                ) : (
+                    <div className="content-box" style={{overflow: 'auto'}}>
+                        <TableCanvas>
+                            <TableHeader name="accounts" sort={setListViewSortBy} view={view} columns={columns}/>
+                            {rows.map((row, index) => {
+                                return (
+                                    <Row key={index}>
+                                        {columns
+                                            .filter(({isHidden}) => isHidden === false)
+                                            .map((item) => getCell(row, item))}
+                                    </Row>
+                                );
+                            })}
+                        </TableCanvas>
+                    </div>
+                )}
+            </div>
+        </>
+    );
 };
